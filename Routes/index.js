5f6e289cc507af3238fbc5524576df6db061ff96
Cromwell Patrick Davey <cromwellpatrick.davey@gmail.com>
2023-01-25 03:04:36 +0800

    Adds comments per http request

--- Routes/index.js
+++ Routes/index.js
@@ -3,20 +3,26 @@ import { GetPeople, InsertPerson, UpdatePerson, DeletePerson} from "../Controlle
 
 const router = Router();
 
-
+// Returns all person in the database
 router.get("/", async(req, res) => {
     const data = await GetPeople();
     res.json(data);
 })
+
+// Returns the added person
 router.post("/addPerson", async(req, res) => {
     const person = await InsertPerson(req);
     res.json(person)
 })
+
+// Returns the new data for a person
 router.put("/changeData/:Id", async(req, res) => {
     const newData = await UpdatePerson(req);
     console.log(newData);
     res.json(newData);
 })
+
+// Returns the deleted person
 router.delete("/removePerson/:Id", async(req, res) => {
     const deletedPerson = await DeletePerson(req);
     res.json(deletedPerson)
